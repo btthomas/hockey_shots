e51a1146d96609dba457caf9588ece3a9b757770
Blake <bthomas@onlocationinc.com>
2016-02-28 16:43:16 -0500

    crunch is now a module

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 var s = require('./lib/scrape.js');
+var c = require('./lib/crunchGame.js');
 
-var game = '20930';
+var game = '020930';
 
 s.scrapeOneGame(game, function(err) {
   if(err) console.error('bad scrape: ' + game, err);
+});
+
+c.getOneGame(game, function(err, data) {
+  if(err) {console.error('couldnt getOneGame: ' + game, err);}
+  else {
+    console.log(data);
+  }
 });
