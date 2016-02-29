135f2674f3d73f6055d293cdcf778ed7814f934b
Blake <bthomas@onlocationinc.com>
2016-02-28 19:02:59 -0500

    Procfile for heroku

--- server.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-var exports = module.exports = {};
-
-const fs = require('fs');
-const _  = require('lodash');
-const d3 = require('d3');
-const express = require('express');
-const app = express();
