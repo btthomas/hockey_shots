0f1bdba001ef334ba812e20373890caeca93d00e
Blake <bthomas@onlocationinc.com>
2016-03-15 18:15:30 -0400

    added psql

--- app.js
+++ app.js
@@ -17,3 +17,22 @@ app.use(require('./routes'));
 app.listen(port, function() {
   console.log("Listening on port " + port);     
 }); 
+
+var pg = require('pg');
+
+app.get('/db', function (req, res) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if(err) {
+      console.error(err);
+      res.send('Error: ' + err);
+    } else {
+      client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err)
+         { console.error(err); res.send("Error " + err); }
+        else
+         { res.render('pages/db', {results: result.rows} ); }
+      });
+    }
+  });
+})
