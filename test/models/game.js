24aff9c5da6af665c3b7c51ca0707a8c2c4787aa
Blake <bthomas@onlocationinc.com>
2016-03-16 22:20:43 -0400

    added in models and schema for the games db

---
+++ test/models/game.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const Game = require('../../models/game.js');
+
+const testGame = {
+  id: 2,
+  home: 'WSH',
+  away: 'PIT',
+  date: '2016-02-02'
+};
+
+Game.insertGame(testGame, function(err, dat) {
+  console.log(err);
+  console.log(dat);
+});
